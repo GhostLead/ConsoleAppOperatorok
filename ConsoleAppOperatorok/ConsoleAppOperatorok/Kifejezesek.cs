be29e3a942d198e193628dcefbeb734868ab5231
GhostLead <72067053+GhostLead@users.noreply.github.com>
2023-05-24 20:26:10 +0200

    egyaltalan nem tokeletes

---
+++ ConsoleAppOperatorok/ConsoleAppOperatorok/Kifejezesek.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppOperatorok
+{
+    internal class Kifejezesek
+    {
+
+        int szam1;
+        string szoveg;
+        int szam2;
+
+        public Kifejezesek(string TXTsor) {
+
+            var mezok = TXTsor.Split();
+            this.szam1 = int.Parse(mezok[0]);
+            this.szoveg = mezok[1];
+            this.szam2 = int.Parse(mezok[2]);
+
+        }
+        public Kifejezesek(int szam1, string szoveg, int szam2)
+        {
+            this.szam1 = szam1;
+            this.szoveg = szoveg;
+            this.szam2 = szam2;
+        }
+
+        public int Szam1 { get => szam1; set => szam1 = value; }
+        public string Szoveg { get => szoveg; set => szoveg = value; }
+        public int Szam2 { get => szam2; set => szam2 = value; }
+    }
+}
---
+++ ConsoleAppOperatorok/ConsoleAppOperatorok/Program.cs
@@ -0,0 +1,56 @@
+﻿
+using ConsoleAppOperatorok;
+
+List<Kifejezesek> kifejezesek = new List<Kifejezesek>();
+
+StreamReader sr = new StreamReader("Kifejezesek.txt");
+
+while (!sr.EndOfStream)
+{
+    Kifejezesek kifejezes = new Kifejezesek(sr.ReadLine());
+    kifejezesek.Add(kifejezes);
+}
+
+sr.Close();
+
+// 2.feladat
+Console.WriteLine("Kifejezések száma: " + kifejezesek.Count.ToString());
+
+// 3. feladat
+Console.WriteLine(kifejezesek.Count(x => x.Szoveg == "mod"));
+
+// 4. feladat
+bool van = kifejezesek.Exists(x => x.Szam1 % 10 == 0 && x.Szam2 % 10 == 0);
+if (van)
+{
+    Console.WriteLine("Van ilyen kifejezés!");
+}
+else
+{
+    Console.WriteLine("Nincs ilyen kifejezés!");
+}
+
+// 5. feladat
+var valami = kifejezesek.GroupBy(x => x.Szoveg).ToList();
+
+
+// 8. feladat
+
+string fileName = "eredmeny.txt";
+try
+{
+    StreamWriter sw = new StreamWriter(fileName);
+
+    foreach (var item in kifejezesek)
+    {
+        sw.WriteLine($"{item.Szam1} {item.Szoveg} {item.Szam2}");
+    }
+    Console.WriteLine("Sikeres volt a fájlba írás!");
+    sw.Close();
+}
+catch (Exception hiba)
+{
+    Console.WriteLine(hiba.Message);
+}
+
+
